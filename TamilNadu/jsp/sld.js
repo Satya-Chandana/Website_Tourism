2055242bf930ac3f806dffb510d27af4ab9943f8
Satya-Chandana <msatyachandana@gmail.com>
2021-08-15 19:10:03 +0530

    Tamil Nadu

---
+++ TamilNadu/jsp/sld.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    // owl-carousel blog
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        dots:true
+    });
+
+    AOS.init();
+
+    
+});
